4f98148751b94fe5be4588601a428abf95ac42d3
Julius Müller <julius.b.mueller@gmail.com>
2023-08-19 00:14:19 +0200

    update frontend

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const gyroOutput = document.getElementById("gyro-output")
 const socket = new WebSocket("wss://hackathon-backend-l22i.onrender.com/")
 
-window.addEventListener("deviceorientation", event => {
-    console.log(event.alpha)
-    gyroOutput.innerText = event.alpha
-    socket.send(JSON.stringify({ alpha: event.alpha }))
-})
+socket.onopen = () => {
+    console.log("connection established")
+    window.addEventListener("deviceorientation", event => {
+        console.log(event.alpha)
+        gyroOutput.innerText = event.alpha
+        socket.send(JSON.stringify({ alpha: event.alpha }))
+    })
+}
+
+socket.onmessage = msg => {
+    console.log(msg)
+}
