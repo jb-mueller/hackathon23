91a7f13a6ec57cb3d6a597369d19b085e1615a91
Julius Müller <julius.b.mueller@gmail.com>
2023-08-19 01:41:22 +0200

    use uuids

--- backend/main.js
+++ backend/main.js
@@ -1,20 +1,42 @@
 import { WebSocketServer } from 'ws'
+import {v4 as uuidv4 } from 'uuid'
 
 const PORT = process.env.PORT || 8080
 
 const wss = new WebSocketServer({ port: PORT })
 
-let counter = 0
+const clients = {}
+let unity = null
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', ws => {
     ws.on('message', function message(msg) {
-        console.log("msg received: " + msg)
+        console.log(`message received: ${msg}`)
+        if (msg.toString() === "UNITY") {
+            unity = ws
+        } else {
+            const msgJSON = JSON.parse(msg.toString())
+            if (msgJSON.client) {
+                if (!Object.keys(clients).includes(msgJSON.client)) {
+                    const newUUID = uuidv4()
+                    console.log("new client connected, uuid: " + newUUID)
+                    clients[newUUID] = ws
+                    clients[newUUID].connected = true
+                    ws.send(JSON.stringify({ uuid: newUUID }))
+                    console.log("connected clients: " + Object.keys(clients).length)
+                } else {
+                    clients[msgJSON.client].connected = true
+                }
+                if (unity) {
+                    // TODO: send uuid of new client to unity
+                }
+            }
+        }
     })
-    ws.send("Hello!")
-    setInterval(() => {
-        ws.send("counter: " + counter)
-        counter += 1
-        console.log("sent counter: " + counter)
-    }, 2000)
-    console.log("sent Hello!")
+
+    ws.on('close', () => {
+        const closeUUID = Object.keys(clients).find(key => clients[key] === ws)
+        if (closeUUID) {
+            clients[closeUUID].connected = false
+        }
+    })
 })
--- main.js
+++ main.js
@@ -1,15 +1,31 @@
 const gyroOutput = document.getElementById("gyro-output")
-const socket = new WebSocket("wss://hackathon-backend-l22i.onrender.com/")
+const socket = new WebSocket("ws://localhost:8080") //("wss://hackathon-backend-l22i.onrender.com/")
+
+const cookies = {}
+const cookiesRaw = document.cookie.split("; ")
+cookiesRaw.forEach(cookie => {
+    [key, value] = cookie.split("=")
+    cookies[key] = value
+})
+console.log(cookies)
 
 socket.onopen = () => {
     console.log("connection established")
+    if (cookies.uuid) {
+        socket.send(JSON.stringify({ client: cookies.uuid }))
+    } else {
+        socket.send(JSON.stringify({ client: "new" }))
+    }
     window.addEventListener("deviceorientation", event => {
         console.log(event.alpha)
         gyroOutput.innerText = event.alpha
-        socket.send(JSON.stringify({ alpha: event.alpha }))
+        //socket.send(JSON.stringify({ alpha: event.alpha }))
     })
 }
 
 socket.onmessage = msg => {
-    console.log(msg)
+    const msgJSON = JSON.parse(msg.data)
+    if (msgJSON.uuid) {
+        document.cookie = "uuid=" + msgJSON.uuid
+    }
 }
