796060f309b88012dfa70ef1fde0c26753f34332
Julius Müller <julius.b.mueller@gmail.com>
2023-08-19 03:51:11 +0200

    record audio

--- main.js
+++ main.js
@@ -37,3 +37,49 @@ socket.onmessage = msg => {
 setInterval(() => {
     socket.send(JSON.stringify({ gyro }))
 }, 100);
+
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    console.log("getUserMedia supported.")
+    navigator.mediaDevices.getUserMedia({
+        audio: true
+    }).then(stream => {
+        const mediaRecorder = new MediaRecorder(stream)
+        document.getElementById('record').addEventListener('click', () => {
+            mediaRecorder.start()
+            console.log(mediaRecorder.state)
+            console.log("start media recording")
+        })
+        let chunks = []
+        mediaRecorder.ondataavailable = e => {
+            chunks.push(e.data)
+        }
+        document.getElementById('stop').addEventListener('click', () => {
+            mediaRecorder.stop()
+            console.log(mediaRecorder.state)
+            console.log("recorder stopped")
+        })
+        mediaRecorder.onstop = e => {
+            console.log("recorder stopped")
+
+            const clipName = prompt("Enter a name for your sound clip")
+            const clipContainer = document.createElement("article")
+            const clipLabel = document.createElement("p")
+            const audio = document.createElement("audio")
+
+            audio.setAttribute("controls", "")
+            clipLabel.innerHTML = clipName
+            clipContainer.appendChild(audio)
+            clipContainer.appendChild(clipLabel)
+            document.body.appendChild(clipContainer)
+
+            const blob = new Blob(chunks, {type: "audio/ogg; codecs=opus"})
+            chunks = []
+            const audioURL = window.URL.createObjectURL(blob)
+            audio.src = audioURL
+        }
+    }).catch(err => {
+        console.error(`the following getUserMedia error occured: ${err}`)
+    })
+} else {
+    console.log("getUserMedia not supported on your browser")
+}
